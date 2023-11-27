6b0bc04cfc6fc416686a370ced0dd55a9d35d875
Okoro91 <mi.fracri91@gmail.com>
2023-11-27 18:40:43 +0100

    solved problem 10

--- problems/10-yeller-map.js
+++ problems/10-yeller-map.js
@@ -14,6 +14,8 @@ console.log(yeller(['kiwi', 'orange', 'mango'])); // [ 'KIWI!', 'ORANGE!', 'MANG
 
 let yeller = function(words) {
   // Your code here
+  let yelled = words.map(word => word.toUpperCase() + '!');
+  return yelled;
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -22,4 +24,4 @@ try {
   module.exports = yeller;
 } catch (e) {
   module.exports = null;
-}
+}
