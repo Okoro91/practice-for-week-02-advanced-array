85c1de2145a8aba04af4cbcec1def0c53dee350a
Okoro91 <mi.fracri91@gmail.com>
2023-11-27 16:27:18 +0100

    solved problem 04

--- problems/04-remove-e-words-filter.js
+++ problems/04-remove-e-words-filter.js
@@ -14,6 +14,11 @@ console.log(removeEWords('Enter the building')); // 'building'
 
 let removeEWords = function(sentence) {
     // Your code here
+    let words = sentence.split(' ');
+
+    let ewords = words.filter(word => !(word.includes('e')))
+
+    return ewords.join(' ');
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -22,4 +27,4 @@ try {
   module.exports = removeEWords;
 } catch (e) {
   module.exports = null;
-}
+}
