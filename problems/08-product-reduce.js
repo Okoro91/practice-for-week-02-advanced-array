cb0963e2fbf3c637df1ff61c8c0ccd0233ed5e34
Okoro91 <mi.fracri91@gmail.com>
2023-11-27 17:19:35 +0100

    solved problem 08

--- problems/08-product-reduce.js
+++ problems/08-product-reduce.js
@@ -17,6 +17,8 @@ console.log(productWithReduce([4, 3])); // 12
 
 let productWithReduce = function(nums) {
     // Your code here
+    let product = nums.reduce((prod, num) => prod * num);
+    return product;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -25,4 +27,4 @@ try {
   module.exports = productWithReduce;
 } catch (e) {
   module.exports = null;
-}
+}
