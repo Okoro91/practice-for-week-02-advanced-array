55630bb57098ce58f3468a238f3da04bc3be405a
Okoro91 <mi.fracri91@gmail.com>
2023-11-28 07:18:01 +0100

    solved problem 15

--- problems/15-choose-primes.js
+++ problems/15-choose-primes.js
@@ -20,10 +20,27 @@ console.log(choosePrimes([5, 6, 4, 11, 2017])); // [ 5, 11, 2017 ]
 
 let isPrime = function(num) {
     // Your code here
+    if (num < 2) {
+        return false;
+    }
+
+    for(let i = 2; i < num; i++){
+
+        if(num % i === 0){
+            return false;
+        }
+    }
+    return true;
 }
+
+
 
 let choosePrimes = function(nums) {
     // Your code here
+
+    let primeNum = nums.filter(num => isPrime(num));
+
+    return primeNum;
 }
 
 // alternative solution using Array.filter
@@ -37,4 +54,4 @@ try {
     module.exports = choosePrimes;
 } catch (e) {
     module.exports = null;
-}
+}
